b08ed8e796183a5be4fcd65fa7dec59ee2380a2a
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-12-01 01:21:40 +0500

    Task4

---
+++ Tyuiu.KurbanovFA.Sprint6.Task4.V24.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.KurbanovFA.Sprint6.Task4.V24.Lib
+{
+    public class DataService : ISprint6Task4V24
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] massive = new double[11];
+            int count = 0;
+            double result = 0;
+            for (double x = startValue; x <= stopValue; x++)
+            {
+                result = x + 1.2 != 0 ? (Math.Sin(x) / (x + 3.2)) - Math.Sin(x) * 2 - 2 * x : 0;
+                massive[count] = Math.Round(result, 2);
+                count++;
+            }
+            string outputFile = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask4V24.txt");
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                writer.WriteLine(result);
+            }
+            return massive;
+        }
+    }
+}
---
+++ Tyuiu.KurbanovFA.Sprint6.Task4.V24/FormMain.cs
@@ -0,0 +1,17 @@
+using Tyuiu.KurbanovFA.Sprint6.Task4.V24.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint6.Task4.V24
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
