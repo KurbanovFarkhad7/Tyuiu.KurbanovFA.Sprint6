064785cc4ad1f78a8bf32aaf2ea520fbb1653afc
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-30 23:47:16 +0500

    Task3

---
+++ Tyuiu.KurbanovFA.Sprint6.Task3.V12.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.KurbanovFA.Sprint6.Task3.V12.Lib
+{
+    public class DataService : ISprint6Task3V12
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int[,] resultMatrix = new int[,] { };
+            for (int y = 0; y < matrix.GetLength(0); y++)
+            {
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    if (matrix[y, x] == 0 && y == 0)
+                    {
+                        matrix[y, x] = 0;
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
