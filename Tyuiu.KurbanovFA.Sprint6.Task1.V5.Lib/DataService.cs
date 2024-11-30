4b25653e2b7f27bcd8c63eab650e59ffcdb14882
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-30 21:41:41 +0500

    Task1

--- Tyuiu.KurbanovFA.Sprint6.Task1.V5.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint6.Task1.V5.Lib/DataService.cs
@@ -2,11 +2,25 @@
 
 namespace Tyuiu.KurbanovFA.Sprint6.Task1.V5.Lib
 {
-    public class DataService : ISprint6Task0V5
+    public class DataService : ISprint6Task1V5
     {
-        public double Calculate(int x)
+        public double[] GetMassFunction(int startValue, int stopValue)
         {
             DataService ds = new DataService();
+            double[] massive = new double[11];
+            int count = 0;
+            for (double i = startValue; i <= stopValue; i += 1)
+            {
+                double x = -5 + i;
+                double result = CalculateFunction(x);
+                // Вычисляем значение функции, добавляем проверку на деление на 0
+                massive[count] = Math.Round(result, 2);
+                count++;
+            }
+            return massive;
+        }
+        static double CalculateFunction(double x)
+        {
             double numerator = Math.Cos(2 * x);
             double denominator = 2;
             // Проверка на деление на ноль (хотя в данном случае деление на 2 невозможно, но для примера)
@@ -17,5 +31,7 @@ namespace Tyuiu.KurbanovFA.Sprint6.Task1.V5.Lib
 
             return Math.Sin(x) + numerator / denominator - 1.5 * x;
         }
+
+        
     }
 }
