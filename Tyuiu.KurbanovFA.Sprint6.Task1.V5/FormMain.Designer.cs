ec601d1922e79f3e95df1d078857cadf3c504d2e
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-30 22:23:47 +0500

    Task1

--- Tyuiu.KurbanovFA.Sprint6.Task1.V5/FormMain.Designer.cs
+++ Tyuiu.KurbanovFA.Sprint6.Task1.V5/FormMain.Designer.cs
@@ -29,21 +29,73 @@
         private void InitializeComponent()
         {
             textBoxResult_KFA = new TextBox();
+            buttonResult_KFA = new Button();
+            textBoxStartStep_KFA = new TextBox();
+            textBoxStopStep_KFA = new TextBox();
+            labelStartStep_KFA = new Label();
+            labelStopStep_KFA = new Label();
             SuspendLayout();
             // 
             // textBoxResult_KFA
             // 
-            textBoxResult_KFA.Location = new Point(591, 12);
+            textBoxResult_KFA.Location = new Point(227, 12);
             textBoxResult_KFA.Multiline = true;
             textBoxResult_KFA.Name = "textBoxResult_KFA";
             textBoxResult_KFA.Size = new Size(197, 426);
             textBoxResult_KFA.TabIndex = 0;
             // 
+            // buttonResult_KFA
+            // 
+            buttonResult_KFA.Location = new Point(12, 76);
+            buttonResult_KFA.Name = "buttonResult_KFA";
+            buttonResult_KFA.Size = new Size(209, 95);
+            buttonResult_KFA.TabIndex = 1;
+            buttonResult_KFA.Text = "DoSomeMagic";
+            buttonResult_KFA.UseVisualStyleBackColor = true;
+            buttonResult_KFA.Click += buttonResult_KFA_Click;
+            // 
+            // textBoxStartStep_KFA
+            // 
+            textBoxStartStep_KFA.Location = new Point(12, 35);
+            textBoxStartStep_KFA.Name = "textBoxStartStep_KFA";
+            textBoxStartStep_KFA.Size = new Size(100, 23);
+            textBoxStartStep_KFA.TabIndex = 2;
+            // 
+            // textBoxStopStep_KFA
+            // 
+            textBoxStopStep_KFA.Location = new Point(121, 35);
+            textBoxStopStep_KFA.Name = "textBoxStopStep_KFA";
+            textBoxStopStep_KFA.Size = new Size(100, 23);
+            textBoxStopStep_KFA.TabIndex = 3;
+            // 
+            // labelStartStep_KFA
+            // 
+            labelStartStep_KFA.AutoSize = true;
+            labelStartStep_KFA.Location = new Point(12, 12);
+            labelStartStep_KFA.Name = "labelStartStep_KFA";
+            labelStartStep_KFA.Size = new Size(54, 15);
+            labelStartStep_KFA.TabIndex = 4;
+            labelStartStep_KFA.Text = "StartStep";
+            // 
+            // labelStopStep_KFA
+            // 
+            labelStopStep_KFA.AutoSize = true;
+            labelStopStep_KFA.Location = new Point(144, 12);
+            labelStopStep_KFA.Name = "labelStopStep_KFA";
+            labelStopStep_KFA.Size = new Size(54, 15);
+            labelStopStep_KFA.TabIndex = 5;
+            labelStopStep_KFA.Text = "StopStep";
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(467, 450);
+            Controls.Add(labelStopStep_KFA);
+            Controls.Add(labelStartStep_KFA);
+            Controls.Add(textBoxStopStep_KFA);
+            Controls.Add(textBoxStartStep_KFA);
+            Controls.Add(buttonResult_KFA);
             Controls.Add(textBoxResult_KFA);
             Name = "FormMain";
             Text = "Form1";
@@ -54,5 +106,10 @@
         #endregion
 
         private TextBox textBoxResult_KFA;
+        private Button buttonResult_KFA;
+        private TextBox textBoxStartStep_KFA;
+        private TextBox textBoxStopStep_KFA;
+        private Label labelStartStep_KFA;
+        private Label labelStopStep_KFA;
     }
 }
--- Tyuiu.KurbanovFA.Sprint6.Task1.V5/FormMain.cs
+++ Tyuiu.KurbanovFA.Sprint6.Task1.V5/FormMain.cs
@@ -1,3 +1,5 @@
+using Tyuiu.KurbanovFA.Sprint6.Task1.V5.Lib;
+
 namespace Tyuiu.KurbanovFA.Sprint6.Task1.V5
 {
     public partial class FormMain : Form
@@ -5,6 +7,33 @@ namespace Tyuiu.KurbanovFA.Sprint6.Task1.V5
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void buttonResult_KFA_Click(object sender, EventArgs e)
+        {
+            DataService ds = new DataService();
+            int startStep = Convert.ToInt32(textBoxStartStep_KFA.Text);
+            int stopStep = Convert.ToInt32(textBoxStopStep_KFA.Text);
+
+            int len = ds.GetMassFunction(startStep, stopStep).Length;
+            double[] valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startStep, stopStep);
+
+            textBoxResult_KFA.Text = "";
+            textBoxResult_KFA.AppendText("+---------+----------+" + Environment.NewLine); // переход на новую строку
+            textBoxResult_KFA.AppendText("+    X    +    Y     +" + Environment.NewLine);
+            textBoxResult_KFA.AppendText("+---------+----------+" + Environment.NewLine);
+
+            string strLine;
+            for (int i = 0; i <= len - 1; i++)
+            {
+                strLine = String.Format("|{0,5:d}   |  {1, 5:f2}   | ", startStep, valueArray[i]);
+                textBoxResult_KFA.AppendText (strLine + Environment.NewLine);
+                startStep++;
+            }
+
+            textBoxResult_KFA.AppendText("+---------+----------+" + Environment.NewLine);
         }
     }
 }
