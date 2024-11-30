97c97447d7b56c3a7f2b7bf694297f8a9e664ee6
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-11-30 22:53:38 +0500

    Task2

---
+++ Tyuiu.KurbanovFA.Sprint6.Task2.V5/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KurbanovFA.Sprint6.Task2.V5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
