0d89dfe5cae3f6989c2449867ca915507e86681b
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-12-01 00:54:09 +0500

    Task3

--- Tyuiu.KurbanovFA.Sprint6.Task3.V12/Program.cs
+++ Tyuiu.KurbanovFA.Sprint6.Task3.V12/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.KurbanovFA.Sprint6.Task3.V12
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
